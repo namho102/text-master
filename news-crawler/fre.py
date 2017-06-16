ab2a822b3801d6bbc5f0132e6e9dd135a4664e95
Hoang Nam <namhoang.td09@gmail.com>
2017-06-16 15:39:21 +0700

    add new crawler :sparkles:

--- news-crawler/fre.py
+++ news-crawler/fre.py
@@ -2,7 +2,7 @@ import string
 from collections import Counter
 
 
-file = open('techcrunch_.txt', 'r')
+file = open('theverge1.txt', 'r')
 text = file.read().decode('utf-8').encode("ascii","ignore")
 out = text.translate(None, string.punctuation)
 # print out
