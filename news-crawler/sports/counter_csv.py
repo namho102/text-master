6c7bdbfd95255ced2491f460976d0f3f0e0c81d5
Hoang Nam <namhoang.td09@gmail.com>
2017-06-17 16:25:29 +0700

    update csv :ok_hand:

--- news-crawler/sports/counter_csv.py
+++ news-crawler/sports/counter_csv.py
@@ -5,7 +5,7 @@ import csv
 
 cnt = Counter()
 
-writefile = open('techcrunch.csv', 'wb')
+writefile = open("/home/foo/pypy/text-master/news-crawler/sports/theguardian.csv", 'wb')
 writer = csv.writer(writefile)
 
 cachedStopWords = ["say", "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost",
@@ -14,7 +14,7 @@ cachedStopWords = ["say", "a", "about", "above", "above", "across", "after", "af
                    "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "just", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "said", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"];
 
 
-file = open('techcrunch_.txt', 'r')
+file = open('/home/foo/pypy/text-master/news-crawler/sports/theguardian.txt', 'r')
 text = file.read().decode('utf-8').encode("ascii", "ignore")
 out = text.translate(None, string.punctuation)
 word_list = out.split()
