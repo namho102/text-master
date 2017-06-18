01322cc31e7807d0c86b12d29726aa759385a969
Hoang Nam <namhoang.td09@gmail.com>
2017-06-18 08:47:27 +0700

    Add NaiveBayesClassifier :sparkles:

---
+++ news-crawler/classifier/classifier.py
@@ -0,0 +1,30 @@
+import random
+
+from textblob.classifiers import NaiveBayesClassifier
+
+file1 = open('sport.txt', 'r')
+text1 = file1.read().decode('utf-8').encode("ascii", "ignore")
+
+file2 = open('tech.txt', 'r')
+text2 = file2.read().decode('utf-8').encode("ascii", "ignore")
+
+sport_list = text1.strip().split('.')
+tech_list = text2.strip().split('.')
+
+
+arr = []
+for i in range(0, 5000):
+    arr.append((sport_list[i], 'Sport'))
+    arr.append((tech_list[i], 'Tech'))
+
+sample = random.sample(arr, 2000)
+
+train = sample[0:1000]
+test = sample[1900:2000]
+
+# print len(train)
+
+cl = NaiveBayesClassifier(train)
+print cl.classify("Reply threads on Facebook are about to get a lot more animated.")
+print cl.classify("Russia can build on opening day win")
+print cl.accuracy(test)
