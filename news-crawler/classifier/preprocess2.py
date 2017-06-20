0fa4201d44f1889702bed9c0d4cec99edd9738d8
Hoang Nam <namhoang.td09@gmail.com>
2017-06-20 21:23:35 +0700

    add csv

--- news-crawler/classifier/preprocess2.py
+++ news-crawler/classifier/preprocess2.py
@@ -4,13 +4,24 @@ import random
 
 
 def write_csv(topic):
-    with codecs.open('topic' + '.txt','r',encoding='utf8') as f:
-        text = f.read()
+    with open(topic + '.txt','r') as f:
+        text = f.read().decode('utf-8').encode("ascii", "ignore")
 
-    nums = [1, 1, 2, 2, 3, 3, 5]
-    # random.choice(mylist)
-    with open('topic' + '.csv', 'wb') as csvfile:
-        writer = csv.writer(csvfile)
-        for i in range(0, 100):
-            writer.writerow(random.choice(nums), 55)
+        nums = [1, 1, 2, 2, 3, 3, 5]
+        # random.choice(mylist)
+        with open(topic + '.csv', 'wb') as csvfile:
+            writer = csv.writer(csvfile)
 
+            setence_list = text.strip().replace('\n', '').split('.')
+
+            cnt = 0
+            while (cnt < len(setence_list)):
+                para = ''
+                number_setence = random.choice(nums)
+                for i in range(cnt, number_setence + cnt):
+                    para += setence_list[i]
+                cnt += number_setence
+                if len(para):
+                    writer.writerow([para, number_setence])
+
+write_csv('sport')
