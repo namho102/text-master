232b9dbec360b1a2e44f555e62e1993debcf6419
Hoang Nam <namhoang.td09@gmail.com>
2017-08-05 10:17:00 +0700

    Add NodeBox linguistics :sparkles:

--- MNB/fre.py
+++ MNB/fre.py
@@ -1,5 +1,7 @@
 from nltk.tokenize import word_tokenize
 from collections import Counter
+import en
+
 
 all_words = []
 with open('words.txt') as f:
@@ -7,7 +9,7 @@ with open('words.txt') as f:
 
 all_words = [x.strip() for x in content]
 
-print(all_words[:10])
+# print(all_words[:10])
 # print(len(words))
 
 file = open('text/tech_.txt', 'r')
@@ -15,20 +17,32 @@ text = file.read().decode('utf-8')
 
 # word_list = word_tokenize(text)
 topic_word = text.lower().replace('.', " ").split()
-
-print(len(topic_word))
-print(len(set(topic_word)))
+# topic_word = [en.noun.singular(word) for word in topic_word]
+# print(len(topic_word))
+# print(len(set(topic_word)))
 
 file = open('new_words.txt', 'a')
 
 new_words = []
 words_set = sorted(set(topic_word))
+
+
 new_words = set(words_set) - set(all_words)
-print(len(new_words))
+# print(len(new_words))
+
+word_count = Counter(topic_word)
+word_count = dict(word_count.items())
+
+word_fre = []
 
 for word in sorted(new_words):
-    file.write('\n')
-    file.write(word.encode('utf-8'))
+    if word_count[word] > 10 and word.isalpha():
+        word_fre.append({'word': word, 'count': word_count[word]})
+        file.write('\n')
+        file.write(word.encode('utf-8'))
+
+for pair in sorted(word_fre, key=lambda k: k['count']) :
+    print pair
 # for word in words_set:
 #     if word not in all_words:
 #         new_words.append(word)
@@ -36,7 +50,7 @@ for word in sorted(new_words):
 #         # file.write(word.encode('utf-8'))
 
 # print(len(new_words))
-# for pair in Counter(word_list).most_common(10):
+# for pair in Counter(topic_word).most_common(100):
 #     print pair
 
 # wordfreq = []
